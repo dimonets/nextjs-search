202134f8cee91c30be085ddd780fc3ba04dfbb3c
Dmytro Yeskin <dmitry.yeskin@gmail.com>
2024-12-02 22:25:20 -0800

    Additional accessibility improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,19 +23,19 @@ export default function Header() {
         </Suspense>
         <ul className="order-3 medium flex text-sm md:space-x-4 md:text-base">
           <li key="0">
-            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700">
+            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700" title="Support">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" /></svg>
               <span className="hidden md:inline">Support</span>
             </Link>
           </li>
           <li key="1">
-            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700">
+            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700" title="Account">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>
               <span className="hidden md:inline">Account</span>
             </Link>
           </li>
           <li key="2">
-            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700">
+            <Link href="/" className="hover:text-gray-400 flex items-center space-x-1 px-3 py-2 text-gray-700" title="Cart">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg>
               <span>(1)</span>
             </Link>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,44 +8,44 @@ export default function Nav() {
     >
       <ul className="mt-4 flex flex-col border border-gray-100 bg-gray-50 px-4 py-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white xl:px-0">
         <li key="0">
-          <Link href="/" className="hover:text-brand-400 block text-gray-700">
+          <Link href="/" className="hover:text-brand-400 block text-gray-700" title="Link 1">
             Link 1
           </Link>
         </li>
         <li key="1">
-          <Link href="/" className="hover:text-brand-400 block text-gray-700">
+          <Link href="/" className="hover:text-brand-400 block text-gray-700" title="Link 2">
             Link 2
           </Link>
         </li>
         <li key="2">
-          <Link href="/" className="hover:text-brand-400 block text-gray-700">
+          <Link href="/" className="hover:text-brand-400 block text-gray-700" title="Link 3">
             Link 3
           </Link>
         </li>
         <li key="3">
-          <Link href="/" className="hover:text-brand-400 block text-gray-700">
+          <Link href="/" className="hover:text-brand-400 block text-gray-700" title="Link 4">
             Link 4
           </Link>
         </li>
         <li key="4">
-          <Link href="/" className="hover:text-brand-400 block text-gray-700">
+          <Link href="/" className="hover:text-brand-400 block text-gray-700" title="Link 5">
             Link 5
           </Link>
         </li>
       </ul>
       <ul className="ml-auto mt-4 flex flex-col bg-gray-50 px-4 py-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white xl:px-0">
         <li key="0">
-          <Link href="/" className="text-brand-400 hover:text-brand-600 block">
+          <Link href="/" className="text-brand-400 hover:text-brand-600 block" title="Link 6">
             Link 6
           </Link>
         </li>
         <li key="1">
-          <Link href="/" className="text-brand-400 hover:text-brand-600 block">
+          <Link href="/" className="text-brand-400 hover:text-brand-600 block" title="Link 7">
             Link 7
           </Link>
         </li>
         <li key="2">
-          <Link href="/" className="text-brand-400 hover:text-brand-600 block">
+          <Link href="/" className="text-brand-400 hover:text-brand-600 block" title="Link 8">
             Link 8
           </Link>
         </li>
