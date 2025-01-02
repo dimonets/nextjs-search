375a417c5cdb350af87a654010908dfcbfafd83e
dimonets <dmitry.yeskin@gmail.com>
2025-01-01 22:33:57 -0800

    Updated packages and cache config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
       dynamic: 30,
     },
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1800, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
