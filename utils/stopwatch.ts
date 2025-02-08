13600c3899cc8ea0a588798162aa8cdc0e7e73a6
dimonets <dmitry.yeskin@gmail.com>
2025-02-08 12:50:23 -0800

    Added math round

--- utils/stopwatch.ts
+++ utils/stopwatch.ts
@@ -1,6 +1,6 @@
 export const stopwatchWrapper = async (promise: Promise<any>) => {
   const startTime = performance.now();
   const resp = await promise;
-  resp.executionTime = performance.now() - startTime;
+  resp.executionTime = Math.round(performance.now() - startTime);
   return resp;
 }
