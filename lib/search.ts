a7fb2e1b2337f678438f7f2432030fca60a529b1
dimonets <dmitry.yeskin@gmail.com>
2025-02-10 13:24:11 -0800

    Step 10: Adjusting query code to use tsv column and tsvectors for search (optional step).

--- lib/search.ts
+++ lib/search.ts
@@ -52,6 +52,7 @@ export async function getFacets(props: SearchQueryProps): Promise<{ facets: Face
   //'use cache';
   //cacheLife('hours');
 
+  /*
   const conditions: string[] = [],
       values: string[] = [];
 
@@ -75,6 +76,50 @@ export async function getFacets(props: SearchQueryProps): Promise<{ facets: Face
   });
 
   const sql = "SELECT 'categories' AS attribute, REPLACE(CAST(jsonb_array_elements(categories) AS CHARACTER VARYING(99999)), '\"', '') AS value, COUNT(id) AS count FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + " GROUP BY jsonb_array_elements(categories) UNION SELECT 'brand' AS attribute, brand AS value, COUNT(id) AS count FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + " GROUP BY brand UNION SELECT 'type' AS attribute, type AS value, COUNT(id) AS count FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + " GROUP BY type UNION SELECT 'price_range' AS attribute, price_range AS value, COUNT(id) AS count FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + " GROUP BY price_range UNION SELECT 'rating' AS attribute, CAST(rating AS CHARACTER VARYING(99999)) AS value, COUNT(id) AS count FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + " GROUP BY rating ORDER BY count DESC";
+  */
+
+  const conditions: string[] = [],
+      conditions2: string[] = [],
+      conditions3: string[] = [],
+      conditions4: string[] = [],
+      values: string[] = [];
+
+  // If we have less than 2 different facets selected then use more complex logic with multiple values in next queries
+  const useComplexFacets = !(Object.values(props).filter((value) => Array.isArray(value) && value.length > 0).length > 2);
+
+  Object.keys(props).map((key: string) => {
+    if (Array.isArray(props[key as keyof SearchQueryProps])) {
+      const or: string[] = [];
+      const or2: string[] = [];
+      const and: string[] = [];
+      (props[key as keyof SearchQueryProps] as string[]).map((item: string | number) => {
+        or.push(`(tsv @@ ''${key}\\:' || $${(values.length + 1)}::text || '''::tsquery)`);
+        values.push(typeof item === 'string' ? item.replace(/\s/g,"\\ ").replace(/\&/g,"\\&") : item.toString());
+        if (useComplexFacets && !props['query']) {
+          or2.push(`split_part(word, ':', 2) = $${(values.length + 1)}::text`);
+          and.push(`split_part(word, ':', 2) <> $${(values.length + 1)}::text`);
+          values.push(typeof item === 'string' ? item : item.toString());
+        }
+      });
+      conditions.push('(' + or.join(' OR ') + ')');
+      if (useComplexFacets && !props['query']) {
+        conditions2.push(`(split_part(word, ':', 1) = '${key}'` + " AND (" + or2.join(' OR ') + '))');
+        conditions3.push(`(split_part(word, ':', 1) <> '${key}')`);
+        conditions4.push(`(split_part(word, ':', 1) = '${key}'` + " AND " + and.join(' AND ') + ')');
+      }
+    } else if (key === 'query' && props[key]) {
+      // TODO: This needs to be improved to protect from SQL injections
+      conditions.push('((LOWER(brand) LIKE \'\'%' + props[key].toLowerCase() + '%\'\') OR (LOWER(name) LIKE \'\'%' + props[key].toLowerCase() + '%\'\'))');
+      //conditions.push(`((LOWER(brand) LIKE $${(values.length + 1)}) OR (LOWER(name) LIKE $${(values.length + 1)}))`);
+      //values.push(props[key].toLowerCase() + '%');
+    }
+  });
+
+  const sql = conditions4.length == 2
+    ? "SELECT split_part(word, ':', 1) AS attribute, split_part(word, ':', 2) AS value, ndoc AS count FROM ts_stat('SELECT tsv FROM products WHERE " + conditions.join(' AND ') + "') WHERE (" + conditions2.join(' OR ') + ") OR (" + conditions3.join(' AND ') + ") UNION DISTINCT SELECT split_part(word, ':', 1) AS attribute, split_part(word, ':', 2) AS value, ndoc AS count FROM ts_stat('SELECT tsv FROM products WHERE " + conditions.join(' OR ') + "') WHERE (" + conditions4.join(' OR ') + ") ORDER BY count DESC, value ASC"
+    : conditions4.length == 1 
+      ? "SELECT split_part(word, ':', 1) AS attribute, split_part(word, ':', 2) AS value, ndoc AS count FROM ts_stat('SELECT tsv FROM products WHERE " + conditions.join(' AND ') + "') WHERE (" + conditions2.join(' OR ') + ") OR (" + conditions3.join(' AND ') + ") UNION DISTINCT SELECT split_part(word, ':', 1) AS attribute, split_part(word, ':', 2) AS value, ndoc AS count FROM ts_stat('SELECT tsv FROM products') WHERE (" + conditions4.join(' OR ') + ") ORDER BY count DESC, value ASC"
+      : "SELECT split_part(word, ':', 1) AS attribute, split_part(word, ':', 2) AS value, ndoc AS count FROM ts_stat('SELECT tsv FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + "') ORDER BY count DESC, value ASC";
 
   const { rows } = await dbQuery<Facet>(sql, values);
 
@@ -95,6 +140,7 @@ export async function getResults(props: SearchProps): Promise<{ hits: Hit[] }> {
   Object.keys(props).map((key: string) => {
     if (Array.isArray(props[key as keyof SearchQueryProps])) {
       const or: string[] = [];
+      /*
       (props[key as keyof SearchQueryProps] as string[]).map((item: string | number) => {
         if (key === 'categories') {
           item = '["' + item.toString() + '"]';
@@ -103,6 +149,11 @@ export async function getResults(props: SearchProps): Promise<{ hits: Hit[] }> {
           or.push(`(${key} ${key === 'brand' || key === 'type' ? 'LIKE' : '='} $${(values.length + 1)})`);
         }
         values.push(item.toString());
+      });
+      */
+      (props[key as keyof SearchProps] as string[]).map((item: string | number) => {
+        or.push(`(tsv @@ ('${key}\\:' || $${(values.length + 1)}::text)::tsquery)`);
+        values.push(typeof item === 'string' ? item.replace(/\s/g,"\\ ").replace(/\&/g,"\\&") : item.toString());
       });
       conditions.push('(' + or.join(' OR ') + ')');
     } else if (key === 'query' && props[key]) {
@@ -167,6 +218,7 @@ export async function getResults(props: SearchProps): Promise<{ hits: Hit[] }> {
   });
 
   const sql = "SELECT id, name, brand, image, description, price, sale_price, on_sale, on_clearance, price_range, free_shipping, rating, reviews_total, categories, type FROM products" + (conditions.length > 0 ? " WHERE " + conditions.join(' AND ') : "") + (sort ? " " + sort : "") + (props.size ? " LIMIT " + props.size : "") + (props.page && props.page > 1 ? " OFFSET " + ((props.page - 1) * props.size) : " OFFSET 0");
+
   const { rows } = await dbQuery<Hit>(sql, values);
 
   //await slow(10000);
@@ -184,6 +236,7 @@ export async function getStats(props: SearchQueryProps): Promise<Stat> {
   Object.keys(props).map((key: string) => {
     if (Array.isArray(props[key as keyof SearchQueryProps])) {
       const or: string[] = [];
+      /*
       (props[key as keyof SearchQueryProps] as string[]).map((item: string | number) => {
         if (key === 'categories') {
           item = '["' + item.toString() + '"]';
@@ -192,6 +245,11 @@ export async function getStats(props: SearchQueryProps): Promise<Stat> {
           or.push(`(${key} ${key === 'brand' || key === 'type' ? 'LIKE' : '='} $${(values.length + 1)})`);
         }
         values.push(item.toString());
+      });
+      */
+      (props[key as keyof SearchQueryProps] as string[]).map((item: string | number) => {
+        or.push(`(tsv @@ ('${key}\\:' || $${(values.length + 1)}::text)::tsquery)`);
+        values.push(typeof item === 'string' ? item.replace(/\s/g,"\\ ").replace(/\&/g,"\\&") : item.toString());
       });
       conditions.push('(' + or.join(' OR ') + ')');
     } else if (key === 'query' && props[key]) {
